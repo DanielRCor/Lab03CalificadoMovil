f5fe33758b04f5b04255208741f70dfba116a480
DanielRCor <thedaniel0405@gmail.com>
2024-11-16 12:36:42 -0500

    first commit

---
+++ app/src/main/java/com/rivera/daniel/laboratoriocalificado03/model/Teacher.kt
@@ -0,0 +1,9 @@
+package com.rivera.daniel.laboratoriocalificado03.model
+
+data class Teacher(
+    val name: String,
+    val lastName: String,
+    val phoneNumber: String,
+    val email: String,
+    val imageUrl: String
+)
